2bded8061bbb02f458d2a03e233fc40588e1a2a9
as468579 <as468579@gmail.com>
2020-10-24 18:15:33 +0800

    Initial Commit

---
+++ 00757128_hw3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  00757128_hw3
+//
+//  Created by User18 on 2020/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ 00757128_hw3/_0757128_hw3App.swift
@@ -0,0 +1,17 @@
+//
+//  _0757128_hw3App.swift
+//  00757128_hw3
+//
+//  Created by User18 on 2020/10/24.
+//
+
+import SwiftUI
+
+@main
+struct _0757128_hw3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
